d4e8852c4b1bc1dbf2ca8e6b0cb092237d34a015
panboyu <lihaoming1215@gmail.com>
2022-04-23 09:51:49 +0800

    第二次更新

--- app/src/main/java/pan/bo/yu/my_2022_04_20_test/MainActivity.java
+++ app/src/main/java/pan/bo/yu/my_2022_04_20_test/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         JumpTurn();
-
+        JumpTurn();JumpTurn();JumpTurn();JumpTurn();JumpTurn();
 
     }
 
